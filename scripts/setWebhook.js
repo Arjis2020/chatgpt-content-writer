1640385d43eec5a4742d9f56d9a31d69ddd4570c
Arjis2020 <arjis.chakraborty@gmail.com>
2023-03-09 14:33:47 +0530

    added error handling for auto webhook setup

--- scripts/setWebhook.js
+++ scripts/setWebhook.js
@@ -3,6 +3,11 @@ import { config } from "dotenv"
 config();
 
 (async () => {
-    const { data } = await axios.get(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT}/setWebhook?url=${process.env.CYCLIC_URL}`)
-    console.log(data)
+    if (process.env.TELEGRAM_BOT && process.env.CYCLIC_URL) {
+        const { data } = await axios.get(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT}/setWebhook?url=${process.env.CYCLIC_URL}`)
+        console.log(data)
+    }
+    else {
+        console.log("You probably are building this project for the first time. Nevertheless, setup the required .env variables for your environment and re-run this script / build.")
+    }
 })()    
